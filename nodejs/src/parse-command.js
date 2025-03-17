650007809a0ad48d8dfb5804d4c22e7730d48ed6
Ohlulu Hsih <z30262226@gmail.com>
2025-03-17 22:35:19 +0800

    Hey Syncup!

--- nodejs/src/parse-command.js
+++ nodejs/src/parse-command.js
@@ -1,6 +1,9 @@
 import parser from 'yargs-parser';
 
-const input = "deploy -r slpos"
-
-const argv = parser(input)
+const input = "deploy -r slpos -e staging, preivew"
+const opt = {
+    array: ['e'],
+}
+const argv = parser(input, opt)
+console.log(argv)
 console.log(argv._)
