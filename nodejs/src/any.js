9faf7a6d43e2de3ad15866bffe527db996c45458
Ohlulu Hsih <z30262226@gmail.com>
2025-03-27 09:14:38 +0800

    sync

--- nodejs/src/any.js
+++ nodejs/src/any.js
@@ -1,7 +1,11 @@
-const short = function longUniqueMoreDescriptiveLexicalFoo({ a, b }) {
-    console.log(a, b);
+import superjson from "superjson";
+
+const a = {
+    price: 1265481675487126341237126837354825628735673456788655263462358163546876712,
 };
 
-const 
-short(a);
+const jj = superjson.serialize(a);
+console.log(jj);
 
+const json = JSON.stringify(a);
+console.log(json);
